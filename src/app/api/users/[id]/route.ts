be647324a6190019de2f4f0901397b7b78047c18
Gabriel Landry <gablandry31@gmail.com>
2024-06-24 09:34:44 -0400

    Changes

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -1,21 +1,24 @@
 import prisma from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextRequest) {
-  const id = req.nextUrl.pathname.split("/").pop();
-  const user = await prisma.user.findUnique({ where: { discordId: id } });
+interface UsersIdParams {
+  params: {
+    id: string;
+  };
+}
+
+export async function GET(req: NextRequest, { params }: UsersIdParams) {
+  const user = await prisma.user.findUnique({ where: { discordId: params.id } });
   return NextResponse.json(user);
 }
 
-export async function PATCH(req: NextRequest) {
-  const id = req.nextUrl.pathname.split("/").pop();
+export async function PATCH(req: NextRequest, { params }: UsersIdParams) {
   const { status } = await req.json();
-  const user = await prisma.user.update({ where: { discordId: id }, data: { status } });
+  const user = await prisma.user.update({ where: { discordId: params.id }, data: { status } });
   return NextResponse.json(user);
 }
 
-export async function DELETE(req: NextRequest) {
-  const id = req.nextUrl.pathname.split("/").pop();
-  await prisma.user.delete({ where: { discordId: id } });
+export async function DELETE(req: NextRequest, { params }: UsersIdParams) {
+  await prisma.user.delete({ where: { discordId: params.id } });
   return NextResponse.json({ message: "User deleted" });
 }
