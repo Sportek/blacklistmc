94c6866647aa3f2a68206a27161b24d7d9d4e81e
Gabriel Landry <gablandry31@gmail.com>
2024-06-24 17:37:40 -0400

    Edit user creation

--- src/app/api/users/userSchema.ts
+++ src/app/api/users/userSchema.ts
@@ -4,4 +4,7 @@ import { z } from "zod";
 export const userSchema = z.object({
   discordId: z.string(),
   status: z.enum(Object.values(UserStatus) as [string, ...string[]]),
+  imageUrl: z.string(),
+  displayName: z.string(),
+  username: z.string(),
 });
