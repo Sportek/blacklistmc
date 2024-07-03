a41bf7dfc811dc140c4687b46e8479e5f69eaa8f
Gabriel Landry <gablandry31@gmail.com>
2024-07-03 12:51:32 -0400

    Fix vercel error

---
+++ src/app/api/auth/callback/discord/discord-helper.ts
@@ -0,0 +1,54 @@
+export interface DiscordUser {
+  id: string;
+  username: string;
+  discriminator: string;
+  avatar: string;
+  global_name: string;
+  email: string;
+}
+
+export interface DiscordToken {
+  token_type: string;
+  access_token: string;
+  expires_in: number;
+  refresh_token: string;
+  scope: string;
+}
+
+export interface DiscordCallbackResponse {
+  user: DiscordUser;
+  token: DiscordToken;
+}
+
+export const createAccountIfNotExist = async (user: DiscordUser) => {
+  await updateOrCreateUserInfo(user.id);
+
+  const existingUser = await prisma.user.findUnique({
+    where: {
+      id: user.id,
+    },
+    include: {
+      account: true,
+    },
+  });
+
+  if (!existingUser?.account) {
+    await prisma.account.create({
+      data: {
+        email: user.email,
+        role: "USER",
+        user: {
+          connect: {
+            id: user.id,
+          },
+        },
+      },
+    });
+  }
+};
+
+export type JWTType = "WEB" | "API";
+
+export const generateJWT = (account: Account, type: JWTType, expiresIn: string | undefined = undefined) => {
+  return jwt.sign({ id: account.id, type }, process.env.JWT_SECRET, { expiresIn });
+};
