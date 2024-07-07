7fc974976120ef5542fbd6370fc524453ba5c5b0
Gabriel Landry <gablandry31@gmail.com>
2024-07-07 18:18:56 -0400

    Add identify

--- src/components/auth/discord-login/index.tsx
+++ src/components/auth/discord-login/index.tsx
@@ -6,7 +6,7 @@ interface DiscordLoginProps {
 }
 
 const DiscordLogin = ({ className, children }: DiscordLoginProps) => {
-  const URL = `https://discord.com/oauth2/authorize?client_id=${process.env.NEXT_PUBLIC_AUTH_DISCORD_ID}&response_type=code&redirect_uri=${process.env.NEXT_PUBLIC_AUTH_DISCORD_REDIRECT_URI}&scope=email`;
+  const URL = `https://discord.com/oauth2/authorize?client_id=${process.env.NEXT_PUBLIC_AUTH_DISCORD_ID}&response_type=code&redirect_uri=${process.env.NEXT_PUBLIC_AUTH_DISCORD_REDIRECT_URI}&scope=identify+email`;
 
   return (
     <Link href={URL} className={className}>
