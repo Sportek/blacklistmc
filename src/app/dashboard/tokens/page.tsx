9cb3b3832942b7c103d9824229f10895f50773df
Gabriel Landry <gablandry31@gmail.com>
2024-07-12 16:30:55 -0400

    Stylise the token page

--- src/app/dashboard/tokens/page.tsx
+++ src/app/dashboard/tokens/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Card from "@/components/landing/card";
 import { cn } from "@/lib/utils";
 import { useEffect, useState } from "react";
 
@@ -27,25 +28,30 @@ const TokensPage = () => {
   // Faire un truc pour pas tout l'afficher et pour pouvoir cliquer et le copier
   return (
     <div>
-      {isLoading ? (
-        <p>Chargement...</p>
-      ) : (
-        <div className="flex flex-col px-2 pt-8">
-          <div className="text-xl font-semibold">Token nécessaire pour les requêtes avec un bot</div>
-          <div className="flex items-center">
-            <div className="truncate max-w-xl">{token}</div>
-            <button
-              onClick={handleCopy}
-              className={cn(
-                "px-2 py-1 bg-blue-500 text-white rounded hover:bg-blue-700 transition-colors duration-300",
-                isCopied ? "bg-green-500 hover:bg-green-700" : ""
+ 
+        <div className="w-full flex flex-col gap-2 items-center overflow-y-auto h-screen pt-8 px-2">
+          <div className="max-w-3xl w-full flex flex-col gap-4">
+            <div className="text-2xl font-semibold w-full">Token nécessaire pour les requêtes avec un bot</div>
+            <Card className="flex flex-col gap-2">
+              {isLoading ? (
+                <p>Chargement...</p>
+              ) : (
+                <>
+                  <div className="truncate">{token}</div>
+                  <button
+                    onClick={handleCopy}
+                    className={cn(
+                      "px-2 py-1 bg-blue-500 text-white rounded hover:bg-blue-700 transition-colors duration-300",
+                      isCopied ? "bg-green-500 hover:bg-green-700" : ""
+                    )}
+                  >
+                    {isCopied ? "Copié !" : "Copier"}
+                  </button>
+                </>
               )}
-            >
-              {isCopied ? "Copié !" : "Copier"}
-            </button>
+            </Card>
           </div>
-        </div>
-      )}
+      </div>
     </div>
   );
 };
